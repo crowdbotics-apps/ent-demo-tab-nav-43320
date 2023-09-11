8d7830f1f5f8d7a3c10fe731e9cb43604f3272b7
cb-kozel <132781984+cb-kozel@users.noreply.github.com>
2023-09-10 17:10:50 -0700

    configured react-native-tab-navigation module

--- modules/navigation-tabs/options.js
+++ modules/navigation-tabs/options.js
@@ -2,35 +2,31 @@ import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityI
 import { View, Text } from "react-native";
 import React from "react";
 
-function Feed() {
-  return (
-        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-            <Text>Feed!</Text>
-        </View>
-  );
-}
+import Untitled3 from '../../screens/Untitled3'
+import Untitled4 from '../../screens/Untitled4'
+import Untitled6 from '../../screens/Untitled6'
 
 const tabList = [
   {
-    name: "Feed",
-    component: Feed,
-    label: "Feed",
+    name: "Untitled3",
+    component: Untitled3,
+    label: "Untitled3",
     tabIcon: ({ color, size }) => (
             <MaterialCommunityIcons name="view-gallery" color={color} size={size} />
     )
   },
   {
-    name: "Profile",
-    component: Feed,
-    label: "Profile",
+    name: "Untitled4",
+    component: Untitled4,
+    label: "Untitled4",
     tabIcon: ({ color, size }) => (
             <MaterialCommunityIcons name="account" color={color} size={size} />
     )
   },
   {
-    name: "Notifications",
-    component: Feed,
-    label: "Updates",
+    name: "Untitled6",
+    component: Untitled6,
+    label: "Untitled6",
     tabIcon: ({ color, size }) => (
             <MaterialCommunityIcons name="account-child-outline" color={color} size={size} />
     )
